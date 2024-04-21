d7aee03d8f661d3846b9abcda0053d73230ae0a6
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-20 17:16:38 -0700

    Update Navbar.tsx

--- app/components/main/Navbar.tsx
+++ app/components/main/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             </span>
         </a>
         <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
-            <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+            <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 space-x-8'>
                 <a href="#about-me" className='curser-pointer'>About me</a>
                 <a href="#skills" className='curser-pointer'>Skills</a>
                 <a href="#projects" className='curser-pointer'>Projects</a>
