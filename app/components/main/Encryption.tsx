c9e4914f08aeefd19dffcb54d869929646b51875
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-20 18:36:25 -0700

    Made Some Slight Changes

--- app/components/main/Encryption.tsx
+++ app/components/main/Encryption.tsx
@@ -47,8 +47,11 @@ const Encryption = () => {
             </div>
         </div>
         <div className='absolute z-[20] bottom-[10px] px-[5px]'>
-        <div className='cursive text-[20px] font-medium text-center text-gray-300'>
-            Secure your data with end-to-end encryption
+        <div className='bold text-[20px] font-medium text-center text-gray-300'>
+        Secure your data by using 
+        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> 
+            {" "}Proper Encryption.
+            </span>
         </div>
         </div>
         <div className='w-full flex items-start justify-center absolute'>
--- app/components/sub/SkillText.tsx
+++ app/components/sub/SkillText.tsx
@@ -14,21 +14,21 @@ const SkillText = () => {
             >  
             <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
             <h1 className='Welcome-text text-[13px]'>
-                Think better with Next.js 13
+                Created Using Next.js 13
             </h1>
             </motion.div>
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
+            className='text-[30px] text-white font-medium text-center mb-15 mt-20'
             >
-                Making apps with modern technologies
+                Different Technologies I Have Used
             </motion.div>
-            <motion.div
+            {/* <motion.div
             variants={slideInFromRight(0.5)}
             className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
             >
-                Never miss a task deadline or idea
-            </motion.div>
+                
+            </motion.div> */}
     </div>
   )
 }
