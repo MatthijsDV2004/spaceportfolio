b7847ce3d77ad6aaca941d7619425da555fea84d
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-28 16:49:23 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        <Analytics/>
+        <SpeedInsights/>
         <StarsCanvas />
         <Navbar />
         {children}
